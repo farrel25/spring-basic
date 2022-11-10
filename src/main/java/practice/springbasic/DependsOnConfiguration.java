e48a40b5a2e25cb9aa37f906dd06d3cf757bc962
Farrel Putra <frl25ap@gmail.com>
2022-11-11 06:45:53 +0700

    add @DependsOn annotation implementation

---
+++ src/main/java/practice/springbasic/DependsOnConfiguration.java
@@ -0,0 +1,26 @@
+package practice.springbasic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import practice.springbasic.data.Bar;
+import practice.springbasic.data.Foo;
+
+@Configuration
+@Slf4j
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn(value = {"bar"})
+    public Foo foo() {
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
+    }
+}
---
+++ src/test/java/practice/springbasic/DependsOnTest.java
@@ -0,0 +1,20 @@
+package practice.springbasic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DependsOnTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(DependsOnConfiguration.class);
+    }
+
+    @Test
+    void testDependsOn() {
+    }
+}
