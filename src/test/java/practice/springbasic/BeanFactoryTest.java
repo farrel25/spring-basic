f191154b27c11401fc3eda734b6ecc3af5b9428f
Farrel Putra <frl25ap@gmail.com>
2022-12-28 07:21:55 +0700

    add implementation of BeanfFactory

---
+++ src/test/java/practice/springbasic/BeanFactoryTest.java
@@ -0,0 +1,34 @@
+package practice.springbasic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice.springbasic.data.Foo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BeanFactoryTest {
+    
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScanConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testBeanFactory() {
+
+        ObjectProvider<Foo> fooObjectProvider = applicationContext.getBeanProvider(Foo.class);
+        List<Foo> fooList = fooObjectProvider.stream().collect(Collectors.toList());
+        System.out.println(fooList);
+
+        Map<String, Foo> beans = applicationContext.getBeansOfType(Foo.class);
+        System.out.println(beans);
+    }
+}
